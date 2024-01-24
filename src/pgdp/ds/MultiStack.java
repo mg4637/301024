e1144363e59ecbfa9764f8bf4aa3f33b6902d9ec
go98tag <mert.goturler@tum.de>
2024-01-24 17:52:43 +0100

    commit

--- src/pgdp/ds/MultiStack.java
+++ src/pgdp/ds/MultiStack.java
@@ -8,64 +8,39 @@ public class MultiStack {
     private final ReentrantReadWriteLock lock = new ReentrantReadWriteLock();
 
     public MultiStack() {
-        stacks = new Stack(1);
+        stacks = new Stack(1, lock);
     }
 
     public void push(int val) {
-        lock.writeLock().lock();
-        try {
-            stacks.push(val);
-        } finally {
-            lock.writeLock().unlock();
-        }
+        stacks.push(val);
     }
 
     public int pop() {
-        lock.writeLock().lock();
-        try {
-            if (stacks.isEmpty()) {
-                return Integer.MIN_VALUE;
-            }
-            return stacks.pop();
-        } finally {
-            lock.writeLock().unlock();
+        if (stacks.isEmpty()) {
+            return Integer.MIN_VALUE;
         }
+        return stacks.pop();
     }
 
     public int top() {
-        lock.readLock().lock();
-        try {
-            if (stacks.isEmpty()) {
-                return Integer.MIN_VALUE;
-            }
-            return stacks.top();
-        } finally {
-            lock.readLock().unlock();
+        if (stacks.isEmpty()) {
+            return Integer.MIN_VALUE;
         }
+        return stacks.top();
     }
 
     public int size() {
-        lock.readLock().lock();
-        try {
-            if (stacks.isEmpty()) {
-                return 0;
-            }
-            return stacks.size();
-        } finally {
-            lock.readLock().unlock();
+        if (stacks.isEmpty()) {
+            return 0;
         }
+        return stacks.size();
     }
 
     public int search(int element) {
-        lock.readLock().lock();
-        try {
-            if (stacks.isEmpty()) {
-                return -1;
-            }
-            return stacks.search(element);
-        } finally {
-            lock.readLock().unlock();
+        if (stacks.isEmpty()) {
+            return -1;
         }
+        return stacks.search(element);
     }
 
     @Override
