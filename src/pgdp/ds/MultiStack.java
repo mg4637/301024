d97cbfaa17656f5c553a7e2d08b7935b0940914e
go98tag <mert.goturler@tum.de>
2024-01-24 17:44:00 +0100

    commit

--- src/pgdp/ds/MultiStack.java
+++ src/pgdp/ds/MultiStack.java
@@ -1,47 +1,75 @@
 package pgdp.ds;
 
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
 public class MultiStack {
 
-	private final Stack stacks;
+    private final Stack stacks;
+    private final ReentrantReadWriteLock lock = new ReentrantReadWriteLock();
 
-	public MultiStack() {
-		stacks = new Stack(1);
-	}
+    public MultiStack() {
+        stacks = new Stack(1);
+    }
 
-	public void push(int val) {
-		stacks.push(val);
-	}
+    public void push(int val) {
+        lock.writeLock().lock();
+        try {
+            stacks.push(val);
+        } finally {
+            lock.writeLock().unlock();
+        }
+    }
 
-	public int pop() {
-		if (stacks.isEmpty()) {
-			return Integer.MIN_VALUE;
-		}
-		return stacks.pop();
-	}
+    public int pop() {
+        lock.writeLock().lock();
+        try {
+            if (stacks.isEmpty()) {
+                return Integer.MIN_VALUE;
+            }
+            return stacks.pop();
+        } finally {
+            lock.writeLock().unlock();
+        }
+    }
 
-	public int top() {
-		if (stacks.isEmpty()) {
-			return Integer.MIN_VALUE;
-		}
-		return stacks.top();
-	}
+    public int top() {
+        lock.readLock().lock();
+        try {
+            if (stacks.isEmpty()) {
+                return Integer.MIN_VALUE;
+            }
+            return stacks.top();
+        } finally {
+            lock.readLock().unlock();
+        }
+    }
 
-	public int size() {
-		if (stacks.isEmpty()) {
-			return 0;
-		}
-		return stacks.size();
-	}
+    public int size() {
+        lock.readLock().lock();
+        try {
+            if (stacks.isEmpty()) {
+                return 0;
+            }
+            return stacks.size();
+        } finally {
+            lock.readLock().unlock();
+        }
+    }
 
-	public int search(int element) {
-		if (stacks.isEmpty()) {
-			return -1;
-		}
-		return stacks.search(element);
-	}
+    public int search(int element) {
+        lock.readLock().lock();
+        try {
+            if (stacks.isEmpty()) {
+                return -1;
+            }
+            return stacks.search(element);
+        } finally {
+            lock.readLock().unlock();
+        }
+    }
 
-	@Override
-	public String toString() {
-		return stacks.toString();
-	}
+    @Override
+    public String toString() {
+        return stacks.toString();
+    }
 }
