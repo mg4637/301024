b0c4bf2ba90611bc536af8be57f9e5bbf7a0e8e3
go98tag <mert.goturler@tum.de>
2024-01-24 18:07:04 +0100

    commit

--- src/pgdp/ds/RingBuffer.java
+++ src/pgdp/ds/RingBuffer.java
@@ -1,68 +1,75 @@
 package pgdp.ds;
 
 import java.util.Arrays;
+import java.util.concurrent.Semaphore;
 
 public class RingBuffer {
 
-	private int[] mem;
-	private int in;
-	private int out;
-	private int stored;
+    private int[] mem;
+    private int in;
+    private int out;
+    private int stored;
+    private Semaphore free;
+    private Semaphore occupied = new Semaphore(0);
 
-	public RingBuffer(int capacity) {
-		mem = new int[capacity];
-		in = 0;
-		out = 0;
-		stored = 0;
-	}
+    public RingBuffer(int capacity) {
+        mem = new int[capacity];
+        in = 0;
+        out = 0;
+        stored = 0;
+        free = new Semaphore(capacity);
+    }
 
-	public boolean isEmpty() {
-		return stored == 0;
-	}
+    public boolean isEmpty() {
+        return stored == 0;
+    }
 
-	public boolean isFull() {
-		return stored == mem.length;
-	}
+    public boolean isFull() {
+        return stored == mem.length;
+    }
 
-	public void put(int val) throws InterruptedException {
-		if (isFull()) {
-			return;
-		}
-		mem[in++] = val;
-		in %= mem.length;
-		stored++;
-	}
+    public void put(int val) throws InterruptedException {
+        occupied.acquire();
+        synchronized (this) {
+            mem[in++] = val;
+            in %= mem.length;
+            stored++;
+        }
+        free.release();
+    }
 
-	public int get() throws InterruptedException {
-		if (isEmpty()) {
-			return Integer.MIN_VALUE;
-		}
-		int val = mem[out++];
-		out %= mem.length;
-		stored--;
-		return val;
-	}
+    public int get() throws InterruptedException {
+        free.acquire();
+        int val;
+        synchronized (this) {
+            val = mem[out++];
+            out %= mem.length;
+            stored--;
+        }
+        occupied.release();
+        return val;
+    }
 
-	@Override
-	public String toString() {
-		StringBuilder sb = new StringBuilder();
-		sb.append("RingBuffer := { capacity = ").append(mem.length).append(", out = ").append(out).append(", in = ")
-				.append(in).append(", stored = ").append(stored).append(", mem = ").append(Arrays.toString(mem))
-				.append(", buffer = [");
-		if (!isEmpty()) {
-			if (in >= 0 || in < mem.length) {
-				int i = out;
-				do {
-					sb.append(mem[i]).append(", ");
-					i = (i + 1) % mem.length;
-				} while (i != in);
-				sb.setLength(sb.length() - 2);
-			} else {
-				sb.append("Error: Field 'in' is <").append(in)
-						.append(">, which is out of bounds for an array of length ").append(mem.length);
-			}
-		}
-		sb.append("] }");
-		return sb.toString();
-	}
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("RingBuffer := { capacity = ").append(mem.length).append(", out = ").append(out).append(", in = ")
+                .append(in).append(", stored = ").append(stored).append(", mem = ").append(Arrays.toString(mem))
+                .append(", buffer = [");
+        if (!isEmpty()) {
+            if (in >= 0 || in < mem.length) {
+                int i = out;
+                do {
+                    sb.append(mem[i]).append(", ");
+                    i = (i + 1) % mem.length;
+                } while (i != in);
+                sb.setLength(sb.length() - 2);
+            } else {
+                sb.append("Error: Field 'in' is <").append(in)
+                        .append(">, which is out of bounds for an array of length ").append(mem.length);
+            }
+        }
+        sb.append("] }");
+        return sb.toString();
+    }
 }
